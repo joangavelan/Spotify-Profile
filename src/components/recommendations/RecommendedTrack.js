d2c12f1171272c09ef7dde7ac3bf7ff54df3cbc3
Johan Gavelan <johangavelan@gmail.com>
2021-03-18 17:25:32 -0500

    recommended track divided into small components

--- src/components/recommendations/RecommendedTrack.js
+++ src/components/recommendations/RecommendedTrack.js
@@ -3,34 +3,59 @@ import { getArtists } from '../helpers'
 import { RiPlayMiniFill } from 'react-icons/ri'
 import { useGlobalState } from '../Provider'
 
+const Title = ({track, artists}) => {
+  return (
+    <div className="playlist__track-title">
+      <div className="playlist__track-thumbnail">
+        <img 
+          className="r-thumbnail" 
+          src={track.album?.images[2]?.url}/>
+        <RiPlayMiniFill />
+      </div>
+      <div className="playlist__track-brand">
+        <p className="playlist__track-name">{track.name}</p>
+        <p className="playlist__track-artist">{artists}</p>
+      </div>
+    </div>
+  );
+}
+
+const Name = ({track}) => {
+  return (
+    <div>
+      <p className="playlist__track-album">
+        {track.name}
+      </p>
+    </div>
+  );
+}
+
+const AddButton = ({track, handleTrackAdittion}) => {
+  return (
+    <div>
+      <button 
+        className="playlist__track-add"
+        onClick={() => handleTrackAdittion(track.uri, track.id)}>
+          Add
+      </button>
+    </div>
+  );
+}
+
 const RecommendedTrack = ({track, index, handleTrackClick, handleTrackAdittion}) => {
 
   const [{ selected_track }] = useGlobalState();
 
   const artists = useCallback(getArtists(track.artists), []);
+  const selectedClass = selected_track.field === 'recommended' && selected_track.index === index ? 'selected' : '';
 
   return (
     <div
-    className={`playlist__track grid-row col-3 ${selected_track.field === 'recommended' && selected_track.index === index ? 'selected' : ''}`}
-    onClick={(e) => handleTrackClick(track.external_urls.spotify, index, e)}>
-      <div className="playlist__track-title">
-        <div className="playlist__track-thumbnail">
-          <img className="r-thumbnail" src={track.album?.images[2]?.url}/>
-          <RiPlayMiniFill />
-        </div>
-        <div className="playlist__track-brand">
-          <p className="playlist__track-name">{track.name}</p>
-          <p className="playlist__track-artist">{artists}</p>
-        </div>
-      </div>
-      <div><p className="playlist__track-album">{track.name}</p></div>
-      <div>
-        <button 
-        className="playlist__track-add"
-        onClick={() => handleTrackAdittion(track.uri, track.id)}>
-          Add
-        </button>
-      </div>
+      className={`playlist__track grid-row col-3 ${selectedClass}`}
+      onClick={(e) => handleTrackClick(track.external_urls.spotify, index, e)}>
+        <Title track={track} artists={artists}/>
+        <Name track={track}/>
+        <AddButton track={track} handleTrackAdittion={handleTrackAdittion}/>
     </div>
   )
 }
