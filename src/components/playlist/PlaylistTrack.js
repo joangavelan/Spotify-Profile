c827a927d1c0dc8329e9c920cca719bc10aea326
Johan Gavelan <johangavelan@gmail.com>
2021-03-17 23:01:47 -0500

    playlist track component divided into little components for better readability

--- src/components/playlist/PlaylistTrack.js
+++ src/components/playlist/PlaylistTrack.js
@@ -3,6 +3,53 @@ import { getArtists, millsToMinAndSec, clockFormat, getTimeDifference } from '..
 import { AiOutlineDelete } from 'react-icons/ai'
 import { useGlobalState } from '../Provider'
 
+const Numeration = ({index}) => <div>{index + 1}</div>;
+
+const Title = ({item, artists}) => {
+  return (
+    <div className="playlist__track-title-wrapper">
+      <div className="playlist__track-title">
+        <div className="playlist__track-thumbnail">
+          <img 
+            src={item.track.album?.images[2]?.url} 
+            alt={item.track.name}/>
+        </div>
+        <div className="playlist__track-brand">
+          <p className="playlist__track-name">{item.track.name}</p>
+          <p className="playlist__track-artist">{artists}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+const Album = ({item}) => {
+  return (
+    <div>
+      <span className="playlist__track-album">
+        {item.track.album.name}
+      </span>
+    </div>
+  );
+}
+
+const Added = ({added}) => {
+  return (
+    <div>
+      <span>{added}</span>
+    </div>
+  );
+}
+
+const LastColumn = ({item, duration, removeTrack}) => {
+  return (
+    <div>
+      <AiOutlineDelete className="playlist__delete-icon" onClick={() => removeTrack(item.track.uri)}/>
+      <span>{duration}</span>
+    </div>
+  );
+}
+
 const Track = ({item, index, handleTrackClick, removeTrack}) => {
 
   const [{selected_track}] = useGlobalState();
@@ -10,33 +57,17 @@ const Track = ({item, index, handleTrackClick, removeTrack}) => {
   const added = useCallback(getTimeDifference(item.added_at), []);
   const trackDuration = useCallback(millsToMinAndSec(item.track.duration_ms, clockFormat), []);
   const artists = useCallback(getArtists(item.track.artists), []);
+  const selectedClass = selected_track.field === 'playlist' && selected_track.index === index ? 'selected' : '';
 
   return (
-    <div
-      className={`playlist__track grid-row ${selected_track.field === 'playlist' && selected_track.index === index ? 'selected' : ''} `} 
+    <div 
+      className={`playlist__track grid-row ${selectedClass}`} 
       onClick={(e) => handleTrackClick(item.track.external_urls.spotify, index, e)}>
-      <div>{index+1}</div>
-      <div className="playlist__track-title-wrapper">
-        <div className="playlist__track-title">
-          <div className="playlist__track-thumbnail">
-            <img 
-              src={item.track.album?.images[2]?.url} 
-              alt={item.track.name}/>
-          </div>
-          <div className="playlist__track-brand">
-            <p className="playlist__track-name">{item.track.name}</p>
-            <p className="playlist__track-artist">{artists}</p>
-          </div>
-        </div>
-      </div>
-      <div><span className="playlist__track-album">{item.track.album.name}</span></div>
-      <div><span>{added}</span></div>
-      <div>
-        <AiOutlineDelete 
-          className="playlist__delete-icon" 
-          onClick={() => removeTrack(item.track.uri)}/>
-        <span>{trackDuration}</span>
-      </div>
+        <Numeration index={index}/>
+        <Title item={item} artists={artists}/>
+        <Album item={item}/>
+        <Added added={added}/>
+        <LastColumn item={item} duration={trackDuration} removeTrack={removeTrack}/>
     </div>
   )
 }
