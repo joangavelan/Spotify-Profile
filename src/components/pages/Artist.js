876a274a12c413b4c02c66372772e60c023aee25
Johan Gavelan <johangavelan@gmail.com>
2021-03-26 21:57:41 -0500

    artist page done

--- src/components/pages/Artist.js
+++ src/components/pages/Artist.js
@@ -3,6 +3,7 @@ import { useLocation } from 'react-router-dom'
 import Loader from '../loader/Loader';
 import { spotifyApi } from '../spotify'
 import Player from '../player-button/PlayButton'
+import './Artist.scss' 
 
 const Artist = () => {
 
@@ -18,27 +19,38 @@ const Artist = () => {
       })
   }, [artistId])
 
+  const genres = [];
+  
+  if(artist) {
+    artist.genres.map(genre => genres.push(genre));
+  }
+
   return (
     <>
       {artist
-        ? <div>
-            <h1>{artist.name}</h1>
-            <img src={artist.images?.[0]?.url} alt={artist.name}/>
+        ? <div className="artist container-md">
+            <div className="artist__header">
+              <h1 className="artist__name">{artist.name}</h1>
+              <img 
+                className="artist__thumbnail" 
+                src={artist.images?.[0]?.url} 
+                alt={artist.name}/>
+            </div>
+            <div className="artist__stats">
+              <div className="artist__genres">
+                <h3>Genres</h3>
+                <p>{genres.join(', ')}</p>
+              </div>
+              <div className="artist__followers">
+                <h3>Followers</h3>
+                <p>{artist.followers.total}</p>
+              </div>
+              <div className="artist__popularity">
+                <h3>Popularity</h3>
+                <p>{artist.popularity}%</p>
+              </div>
+            </div>
             <Player url={artist.external_urls.spotify}/>
-            <div>
-              <h3>Genres</h3>
-              <ul>
-                {artist.genres.map(genre => <li>{genre}</li>)}
-              </ul>
-            </div>
-            <div>
-              <h3>Popularity:</h3>
-              <span>{artist.popularity}</span>
-            </div>
-            <div>
-              <h3>Followers:</h3>
-              <span>{artist.followers.total}</span>
-            </div>
           </div>
         : <Loader />}
     </>
